6d4ce1440f1372ffbdaf0d270bac3f03ea897350
wangxiaodong <824976164@qq.com>
2020-07-21 16:22:15 +0800

    7-21第一题

---
+++ 7-21leetcode/96不同的二叉搜索树(动态规划).py
@@ -0,0 +1,41 @@
+# 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+#
+#  示例:
+#
+#  输入: 3
+# 输出: 5
+# 解释:
+# 给定 n = 3, 一共有 5 种不同结构的二叉搜索树:
+#
+#    1         3     3      2      1
+#     \       /     /      / \      \
+#      3     2     1      1   3      2
+#     /     /       \                 \
+#    2     1         2                 3
+#  Related Topics 树 动态规划
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [0 for _ in range(n + 1)]
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n + 1):
+            for j in range(1, i + 1):
+                dp[i] += dp[j - 1] * dp[i - j]
+        print(dp)
+        return dp[n]
+
+
+# leetcode submit region end(Prohibit modification and deletion)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    res = solution.numTrees(1)
+    print(res)
